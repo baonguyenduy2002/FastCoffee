ea875fb878b12a78380f505858736750857d5d94
HoangREALER <hoangle1742002@google.com>
2023-01-02 14:26:26 +0700

    Initial menu layouts

---
+++ fastcoffee/src/components/MenuCollapsible/index.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button'
+import Collapse from 'react-bootstrap/Collapse';
+
+import './MenuCollapsible.css';
+
+
+function MenuCollapsible(props) {
+    const { title } = props;
+    const ctrAria = title.toString().replace(" ", "") + "-collapsible";
+    const [open, setOpen] = useState(false);
+  
+    return (
+      <>
+        <div
+          className="collapsible_toggler"
+          onClick={() => setOpen(!open)}
+          aria-controls={ ctrAria }
+          aria-expanded={open}
+        >
+            <div className="content">
+                { title }
+            </div>
+        </div>
+        <Collapse in={open}>
+          <div id={ ctrAria } className="collapsible_area">
+            <Button>Test1</Button>
+            <Button>Test2</Button>
+            <Alert>Test3</Alert>
+            <Button>Test4</Button>
+          </div>
+        </Collapse>
+      </>
+    );
+  }
+  
+export default MenuCollapsible;
--- fastcoffee/src/pages/DashboardPage/Outlets/Menu/index.jsx
+++ fastcoffee/src/pages/DashboardPage/Outlets/Menu/index.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Menu.css";
 
+import { COLORS } from "../../../../assets/constants";
+
+import Example from "../../../../components/MenuCollapsible";
+import MenuCollapsible from "../../../../components/MenuCollapsible";
+
+
+
 function Menu() {
-  return <div className="Menu">Menu</div>;
+
+  const [restaurant, setRestaurant] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const shopImg = require("../../../../assets/image/login_background1.jpg")
+
+  return (
+
+    <div className="Menu">
+      <div className="ShopImage">
+        <img src={shopImg} alt="" />
+      </div>
+
+      <div className="ShopProfile">
+        <div className="ShopInfo">
+          <div className="ShopName">The bruh coffee shop</div>
+          <div className="ShopLocation">
+            <span className="Distance">0.2km - </span>
+            <span className="Address">123/45 Đường Lý Thường Kiệt, Quận 10, Thành phố Hồ Chí Minh</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="ShopMenu">
+        <div className="MustTry">
+          <MenuCollapsible className="MustTry" title="Must try">
+
+          </MenuCollapsible>
+          <MenuCollapsible className="BestSellers" title="Best Sellers">
+            
+          </MenuCollapsible>
+          <MenuCollapsible className="NewDrinks" title="New Drinks">
+            
+          </MenuCollapsible>
+          </div>
+        sdfasd
+        sdfasdf
+        sdfasdf
+      </div>
+    </div>
+  );
 }
+
 
 export default Menu;
