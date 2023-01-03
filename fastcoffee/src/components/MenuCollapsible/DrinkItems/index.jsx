ccd679b2a3174621d0c2db064026e25b24cb2f91
HoangREALER <hoangle1742002@google.com>
2023-01-03 16:40:34 +0700

    Merge branch 3rd Jan

--- fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
+++ fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import AddIcon from '@mui/icons-material/Add'
 
 import './DrinkItems.css';
 
 import { drinkItems } from "../../../controller/data/drinks";
 import { drinksItemImage } from "../../../assets";
+import { COLORS } from "../../../assets/constants";
+import { width } from "@mui/system";
 
+const formatter = new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
 function DrinkItems(props) {
 
     const { list } = props;
@@ -17,18 +28,33 @@ function DrinkItems(props) {
             {drinkItemsList.map((item) => {
                 return (
                     <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={ drinksItemImage.filter(function(el){return el.id === item.Item_ID })[0].image } alt="File corrupted"/>
+                        <Card.Img variant="top" src={drinksItemImage.filter(function (el) { return el.id === item.Item_ID })[0].image} alt="File corrupted" />
                         <Card.Body className="DrinkInfo">
-                            <Card.Title className="DrinkName">{ item.Name }</Card.Title>
+                            <Card.Title className="DrinkName">{item.Name}</Card.Title>
                             <Card.Text className="DrinkDescription">
-                                { item.Description }
+                                {item.Description}
                             </Card.Text>
-                            <Button variant="primary">{ item.Price }</Button>
+                            <Button variant="primary">{formatter.format(item.Price)}</Button>
                         </Card.Body>
                     </Card>
                 );
             })}
+
+            <div style={{ width: '18rem' }}>
+                <Button variant="add-item outline-primary " style={add_item_style}>
+                    <AddIcon className="add-icon"/>
+                </Button>
+            </div>
         </>
     )
 }
+
+const add_item_style = {
+    border: '3px solid ' + COLORS.darkgray,
+    color:  COLORS.darkgray,
+    borderRadius: '50%',
+    height: '60px',
+    width: '60px'
+};
+
 export default DrinkItems;
--- fastcoffee/src/controller/data/drinks.js
+++ fastcoffee/src/controller/data/drinks.js
@@ -63,11 +63,27 @@ export const drinkItems = {
         'Price': 45000,
         'Avalability': 20,
         'Shop_ID': 9
+    }, {
+        'Item_ID': 1,
+        'Name': 'Ristretto Bianco',
+        'Description': 'An espresso like',
+        'Image': 'drink_1.jpg',
+        'Price': 45000,
+        'Avalability': 20,
+        'Shop_ID': 9
     }],
 
 
     
     "menu": [{
+        'Item_ID': 1,
+        'Name': 'Ristretto Bianco',
+        'Description': 'An espresso like',
+        'Image': 'drink_1.jpg',
+        'Price': 45000,
+        'Avalability': 20,
+        'Shop_ID': 9
+    }, {
         'Item_ID': 1,
         'Name': 'Ristretto Bianco',
         'Description': 'An espresso like',
