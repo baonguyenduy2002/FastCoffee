d33c4836b91f409f8bb3cb9ec0c9b964f99cd8ed
mynamesbao11 <baonguyenduy2002@gmail.com>
2023-01-03 22:59:18 +0700

    get order

--- fastcoffee/src/controller/data/order.js
+++ fastcoffee/src/controller/data/order.js
@@ -4,3 +4,8 @@ export const getOrders = async (path) => {
   let response = await instance.get(path);
   return response;
 };
+
+export const makeAccepted = async (path) => {
+  let response = await instance.post(path);
+  return response;
+};
