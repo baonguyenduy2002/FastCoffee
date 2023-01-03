1162152a249f52a958fe3f2eb607f5ad7126610e
HoangREALER <hoangle1742002@google.com>
2023-01-04 04:18:19 +0700

    Mai làm tiếp

--- fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
+++ fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
@@ -8,38 +8,57 @@ import "./DrinkItems.css";
 
 import * as api from "../../../controller/data/drinks";
 import { COLORS } from "../../../assets/constants";
+import AddDialogs from "../../Dialog/Dialog";
 
 const formatter = new Intl.NumberFormat('vi-VN', {
     style: 'currency',
     currency: 'VND',
-  
+
     // These options are needed to round to whole numbers if that's what you want.
     //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
     //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  });
-function DrinkItems(props) {
-  const [drinkItems, setDrinkItems] = useState([]);
+});
 
-  useEffect(() => {
-    api.getDrinks("api/item/get").then((res) => setDrinkItems(res.data));
-  }, []);
+
+const deleteDialog = () => {
+    alert("deleting stuff")
+}
+
+function DrinkItems(props) {
+    const [drinkItems, setDrinkItems] = useState([]);
+    const [openAddPopup, setOpenAddPopup] = useState(false);
+
+	const editDialog = () => {
+		setOpenAddPopup(true);
+	};
+
+    useEffect(() => {
+        api.getDrinks("api/item/get").then((res) => setDrinkItems(res.data));
+    }, []);
 
     return (
         <>
             {drinkItems.map((item) => {
                 return (
-                    <Card style={{ width: '18rem' }}>
-                        <div className="delete"></div>
-                        <CreateIcon className="edit"></CreateIcon>
-                        {/* <Card.Img variant="top" src={drinksItemImage.filter(function (el) { return el.id === item.Item_ID })[0].image} alt="File corrupted" /> */}
-                        <Card.Body className="DrinkInfo">
-                            <Card.Title className="DrinkName">{item.Name}</Card.Title>
-                            <Card.Text className="DrinkDescription">
-                                {item.Description}
-                            </Card.Text>
-                            <Button variant="primary">{formatter.format(item.Price)}</Button>
-                        </Card.Body>
-                    </Card>
+                    <>
+                        <Card style={{ width: '18rem' }}>
+                            <div className="delete" onClick={deleteDialog}></div>
+                            <CreateIcon className="edit" fontSize="small" onClick={editDialog}></CreateIcon>
+                            {/* <Card.Img variant="top" src={drinksItemImage.filter(function (el) { return el.id === item.Item_ID })[0].image} alt="File corrupted" /> */}
+                            <Card.Body className="DrinkInfo">
+                                <Card.Title className="DrinkName">{item.Name}</Card.Title>
+                                <Card.Text className="DrinkDescription">
+                                    {item.Description}
+                                </Card.Text>
+                                <Button variant="primary">{formatter.format(item.Price)}</Button>
+                            </Card.Body>
+                        </Card>
+
+                        <AddDialogs 
+                            openAddPopup={openAddPopup}
+                            setOpenAddPopup={setOpenAddPopup}
+                        />
+                    </>
                 );
             })}
 
@@ -49,7 +68,7 @@ function DrinkItems(props) {
 
 const add_item_style = {
     border: '3px solid ' + COLORS.darkgray,
-    color:  COLORS.darkgray,
+    color: COLORS.darkgray,
     borderRadius: '50%',
     height: '60px',
     width: '60px'
