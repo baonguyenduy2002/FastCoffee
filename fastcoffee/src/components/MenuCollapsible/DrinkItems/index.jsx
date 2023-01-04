13941df6b2da6edc02ad81ac41cfa320cf84f737
mynamesbao11 <baonguyenduy2002@gmail.com>
2023-01-05 02:51:47 +0700

    Done ORDER

--- fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
+++ fastcoffee/src/components/MenuCollapsible/DrinkItems/index.jsx
@@ -1,62 +1,72 @@
-import React, { useState } from "react";
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import AddIcon from '@mui/icons-material/Add'
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import AddIcon from "@mui/icons-material/Add";
 
-import "./DrinkItem.css";
+import "./DrinkItems.css";
 
 import * as api from "../../../controller/data/drinks";
 import { drinksItemImage } from "../../../assets";
 import { COLORS } from "../../../assets/constants";
 import { width } from "@mui/system";
 
-const formatter = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  });
+const formatter = new Intl.NumberFormat("vi-VN", {
+  style: "currency",
+  currency: "VND",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
 function DrinkItems(props) {
   const [drinkItems, setDrinkItems] = useState([]);
 
   useEffect(() => {
-    api.getDrinks("api/item/get").then((res) => setDrinkItems(res.data));
+    api
+      .getDrinks("api/item/get")
+      .then((res) => (res.data ? setDrinkItems(res.data) : setDrinkItems([])));
   }, []);
 
-    return (
-        <>
-            {drinkItemsList.map((item) => {
-                return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={drinksItemImage.filter(function (el) { return el.id === item.Item_ID })[0].image} alt="File corrupted" />
-                        <Card.Body className="DrinkInfo">
-                            <Card.Title className="DrinkName">{item.Name}</Card.Title>
-                            <Card.Text className="DrinkDescription">
-                                {item.Description}
-                            </Card.Text>
-                            <Button variant="primary">{formatter.format(item.Price)}</Button>
-                        </Card.Body>
-                    </Card>
-                );
-            })}
+  return (
+    <>
+      {drinkItems.map((item) => {
+        return (
+          <Card style={{ width: "18rem" }}>
+            {/* <Card.Img
+              variant="top"
+              src={
+                drinksItemImage.filter(function (el) {
+                  return el.id === item.Item_ID;
+                })[0].image
+              }
+              alt="File corrupted"
+            /> */}
+            <Card.Body className="DrinkInfo">
+              <Card.Title className="DrinkName">{item.Name}</Card.Title>
+              <Card.Text className="DrinkDescription">
+                {item.Description}
+              </Card.Text>
+              <Button variant="primary">{formatter.format(item.Price)}</Button>
+            </Card.Body>
+          </Card>
+        );
+      })}
 
-            <div style={{ width: '18rem' }}>
-                <Button variant="add-item outline-primary " style={add_item_style}>
-                    <AddIcon className="add-icon"/>
-                </Button>
-            </div>
-        </>
-    )
+      <div style={{ width: "18rem" }}>
+        <Button variant="add-item outline-primary " style={add_item_style}>
+          <AddIcon className="add-icon" />
+        </Button>
+      </div>
+    </>
+  );
 }
 
 const add_item_style = {
-    border: '3px solid ' + COLORS.darkgray,
-    color:  COLORS.darkgray,
-    borderRadius: '50%',
-    height: '60px',
-    width: '60px'
+  border: "3px solid " + COLORS.darkgray,
+  color: COLORS.darkgray,
+  borderRadius: "50%",
+  height: "60px",
+  width: "60px",
 };
 
 export default DrinkItems;
--- fastcoffee/src/components/OrderCollapsible/OrderTag/index.js
+++ fastcoffee/src/components/OrderCollapsible/OrderTag/index.js
@@ -6,37 +6,469 @@ import moment from "moment";
 import "./OrderTag.css";
 
 import * as api from "../../../controller/data/order";
+import image from "../../../assets/image/drink_1.jpg";
 
 function OrderTag(props) {
   const { title } = props;
   const [orders, setOrders] = useState([]);
+  const [itemList, setItemList] = useState([]);
+  const [menu, setMenu] = useState([
+    {
+      Item_ID: 1,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 2,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 3,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 4,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 5,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 6,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 7,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 8,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 9,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 10,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 11,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 12,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 13,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 14,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 15,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 16,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 17,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 18,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 19,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 20,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 21,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 22,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 23,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 24,
+      Name: "nothing",
+    },
+    {
+      Item_ID: 25,
+      Name: "nothing",
+    },
+  ]);
+  const [state, updateState] = useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+
+  const handleAccept = (id) => {
+    return () => api.makeAccepted(`api/order/accept/${id}`);
+  };
+
+  const handleDeny = (id) => {
+    return () => api.makeNext(`api/order/deny/${id}`);
+  };
+
+  const handleProcess = (id) => {
+    return () => api.makeNext(`api/order/process/${id}`);
+  };
+
+  const handleReady = (id) => {
+    return () => api.makeNext(`api/order/ready/${id}`);
+  };
+
+  const handleFinish = (id) => {
+    return () => api.makeNext(`api/order/finish/${id}`);
+  };
 
   useEffect(() => {
-    api.getOrders("api/order/get").then((res) => setOrders(res.data));
+    api
+      .getOrders("api/order/get")
+      .then((res) => (res.data ? setOrders(res.data) : setOrders([])));
+    api
+      .getOrders_Items(`api/order/get/order_item`)
+      .then((res) => (res.data ? setItemList(res.data) : setItemList([])));
+    api.getOrders_Items(`api/item/get/`).then((res) =>
+      res.data
+        ? setMenu(res.data)
+        : setMenu([
+            {
+              Item_ID: 1,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 2,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 3,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 4,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 5,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 6,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 7,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 8,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 9,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 10,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 11,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 12,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 13,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 14,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 15,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 16,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 17,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 18,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 19,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 20,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 21,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 22,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 23,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 24,
+              Name: "nothing",
+            },
+            {
+              Item_ID: 25,
+              Name: "nothing",
+            },
+          ])
+    );
   }, []);
 
-  if (props.title == "Pending")
+  if (props.title === "Pending")
     return (
       <>
-        {orders.map((item) => {
-          if (item.Status == props.title)
+        {orders.map((order) => {
+          if (order.Status === props.title)
             return (
-              <Card className="OrderCard" style={{ width: "80rem" }}>
-                <Card.Body>
-                  <Card.Title className="OrderName">
-                    # {item.Order_ID}
+              <Card className="card-group OrderCard" style={{ width: "80rem" }}>
+                <Card.Body style={{ width: "500px" }} className="col-md-2">
+                  <Card.Title className="OrderName text">
+                    # {order.Order_ID}
                   </Card.Title>
-                  <Card.Text>
-                    {moment(item.DateTime).date()}/
-                    {moment(item.DateTime).month()}/
-                    {moment(item.DateTime).year()} at{" "}
-                    {moment(item.DateTime).hour()}:
-                    {moment(item.DateTime).minute()}:
-                    {moment(item.DateTime).second()}
+                  <Card.Text className="underline text">
+                    {moment(order.DateTime).date()}/
+                    {moment(order.DateTime).month()}/
+                    {moment(order.DateTime).year()} at{" "}
+                    {moment(order.DateTime).hour()}:
+                    {moment(order.DateTime).minute()}:
+                    {moment(order.DateTime).second()}
                   </Card.Text>
-                  <Button>Accept</Button>
-                  <Button>Deny</Button>
+                  <Card.Text className="text">
+                    Note: {order.Order_note}
+                  </Card.Text>
+                  <Button class="button" onClick={handleAccept(order.Order_ID)}>
+                    Accept
+                  </Button>
+                  <Button class="button" onClick={handleDeny(order.Order_ID)}>
+                    Deny
+                  </Button>
                 </Card.Body>
+                {itemList.map((itemIndex) => {
+                  if (itemIndex.Order_ID == order.Order_ID)
+                    return (
+                      <div class="row row-cols-1 row-cols-md-3">
+                        <Card.Body className="OrderImg">
+                          <Card.Img
+                            className="OrderImg"
+                            src={image}
+                            alt="File interupted"
+                          />
+                          <Card.Text className="maxleng text">
+                            {menu[itemIndex.Item_ID - 1].Name}
+                          </Card.Text>
+                          <Card.Text className="text">
+                            Quantity: {itemIndex.Quantity}
+                          </Card.Text>
+                          <Card.Text className="maxleng text">
+                            Note: {itemIndex.Item_Note}
+                          </Card.Text>
+                        </Card.Body>
+                      </div>
+                    );
+                })}
+              </Card>
+            );
+        })}
+      </>
+    );
+  else if (props.title == "Accepted")
+    return (
+      <>
+        {orders.map((order) => {
+          if (order.Status === props.title)
+            return (
+              <Card className="card-group OrderCard" style={{ width: "80rem" }}>
+                <Card.Body className="col-md-2">
+                  <Card.Title className="OrderName text">
+                    # {order.Order_ID}
+                  </Card.Title>
+                  <Card.Text className="underline text">
+                    {moment(order.DateTime).date()}/
+                    {moment(order.DateTime).month()}/
+                    {moment(order.DateTime).year()} at{" "}
+                    {moment(order.DateTime).hour()}:
+                    {moment(order.DateTime).minute()}:
+                    {moment(order.DateTime).second()}
+                  </Card.Text>
+                  <Card.Text className="text">
+                    Note: {order.Order_note}
+                  </Card.Text>
+                  <Button
+                    class="button"
+                    onClick={handleProcess(order.Order_ID)}
+                  >
+                    Process
+                  </Button>
+                </Card.Body>
+                {itemList.map((itemIndex) => {
+                  if (itemIndex.Order_ID == order.Order_ID)
+                    return (
+                      <div class="row row-cols-1 row-cols-md-3">
+                        <Card.Body className="OrderImg">
+                          <Card.Img
+                            className="OrderImg"
+                            src={image}
+                            alt="File interupted"
+                          />
+                          <Card.Text className="maxleng text">
+                            {menu[itemIndex.Item_ID - 1].Name}
+                          </Card.Text>
+                          <Card.Text className="text">
+                            Quantity: {itemIndex.Quantity}
+                          </Card.Text>
+                          <Card.Text className="maxleng text">
+                            Note: {itemIndex.Item_Note}
+                          </Card.Text>
+                        </Card.Body>
+                      </div>
+                    );
+                })}
+              </Card>
+            );
+        })}
+      </>
+    );
+  else if (props.title == "Processing")
+    return (
+      <>
+        {orders.map((order) => {
+          if (order.Status === props.title)
+            return (
+              <Card className="card-group OrderCard" style={{ width: "80rem" }}>
+                <Card.Body className="col-md-2">
+                  <Card.Title className="OrderName text">
+                    # {order.Order_ID}
+                  </Card.Title>
+                  <Card.Text className="underline text">
+                    {moment(order.DateTime).date()}/
+                    {moment(order.DateTime).month()}/
+                    {moment(order.DateTime).year()} at{" "}
+                    {moment(order.DateTime).hour()}:
+                    {moment(order.DateTime).minute()}:
+                    {moment(order.DateTime).second()}
+                  </Card.Text>
+                  <Card.Text className="text">
+                    Note: {order.Order_note}
+                  </Card.Text>
+                  <Button class="button" onClick={handleReady(order.Order_ID)}>
+                    Ready
+                  </Button>
+                </Card.Body>
+                {itemList.map((itemIndex) => {
+                  if (itemIndex.Order_ID == order.Order_ID)
+                    return (
+                      <div class="row row-cols-1 row-cols-md-3">
+                        <Card.Body className="OrderImg">
+                          <Card.Img
+                            className="OrderImg"
+                            src={image}
+                            alt="File interupted"
+                          />
+                          <Card.Text className="maxleng text">
+                            {menu[itemIndex.Item_ID - 1].Name}
+                          </Card.Text>
+                          <Card.Text className="text">
+                            Quantity: {itemIndex.Quantity}
+                          </Card.Text>
+                          <Card.Text className="maxleng text">
+                            Note: {itemIndex.Item_Note}
+                          </Card.Text>
+                        </Card.Body>
+                      </div>
+                    );
+                })}
+              </Card>
+            );
+        })}
+      </>
+    );
+  else if (props.title == "Ready")
+    return (
+      <>
+        {orders.map((order) => {
+          if (order.Status === props.title)
+            return (
+              <Card className="card-group OrderCard" style={{ width: "80rem" }}>
+                <Card.Body className="col-md-2">
+                  <Card.Title className="OrderName text">
+                    # {order.Order_ID}
+                  </Card.Title>
+                  <Card.Text className="underline text">
+                    {moment(order.DateTime).date()}/
+                    {moment(order.DateTime).month()}/
+                    {moment(order.DateTime).year()} at{" "}
+                    {moment(order.DateTime).hour()}:
+                    {moment(order.DateTime).minute()}:
+                    {moment(order.DateTime).second()}
+                  </Card.Text>
+                  <Card.Text className="text">
+                    Note: {order.Order_note}
+                  </Card.Text>
+                  <Button class="button" onClick={handleFinish(order.Order_ID)}>
+                    Finish
+                  </Button>
+                </Card.Body>
+                {itemList.map((itemIndex) => {
+                  if (itemIndex.Order_ID == order.Order_ID)
+                    return (
+                      <div class="row row-cols-1 row-cols-md-3">
+                        <Card.Body className="OrderImg">
+                          <Card.Img
+                            className="OrderImg"
+                            src={image}
+                            alt="File interupted"
+                          />
+                          <Card.Text className="maxleng text">
+                            {menu[itemIndex.Item_ID - 1].Name}
+                          </Card.Text>
+                          <Card.Text className="text">
+                            Quantity: {itemIndex.Quantity}
+                          </Card.Text>
+                          <Card.Text className="maxleng text">
+                            Note: {itemIndex.Item_Note}
+                          </Card.Text>
+                        </Card.Body>
+                      </div>
+                    );
+                })}
               </Card>
             );
         })}
@@ -45,23 +477,49 @@ function OrderTag(props) {
   else
     return (
       <>
-        {orders.map((item) => {
-          if (item.Status == props.title)
+        {orders.map((order) => {
+          if (order.Status === props.title)
             return (
-              <Card className="OrderCard" style={{ width: "80rem" }}>
-                <Card.Body>
-                  <Card.Title className="OrderName">
-                    # {item.Order_ID}
+              <Card className="card-group OrderCard" style={{ width: "80rem" }}>
+                <Card.Body className="col-md-2">
+                  <Card.Title className="OrderName text">
+                    # {order.Order_ID}
                   </Card.Title>
-                  <Card.Text>
-                    {moment(item.DateTime).date()}/
-                    {moment(item.DateTime).month()}/
-                    {moment(item.DateTime).year()} at{" "}
-                    {moment(item.DateTime).hour()}:
-                    {moment(item.DateTime).minute()}:
-                    {moment(item.DateTime).second()}
+                  <Card.Text className="underline text">
+                    {moment(order.DateTime).date()}/
+                    {moment(order.DateTime).month()}/
+                    {moment(order.DateTime).year()} at{" "}
+                    {moment(order.DateTime).hour()}:
+                    {moment(order.DateTime).minute()}:
+                    {moment(order.DateTime).second()}
+                  </Card.Text>
+                  <Card.Text className="text">
+                    Note: {order.Order_note}
                   </Card.Text>
                 </Card.Body>
+                {itemList.map((itemIndex) => {
+                  if (itemIndex.Order_ID == order.Order_ID)
+                    return (
+                      <div class="row row-cols-1 row-cols-md-3">
+                        <Card.Body className="OrderImg">
+                          <Card.Img
+                            className="OrderImg"
+                            src={image}
+                            alt="File interupted"
+                          />
+                          <Card.Text className="maxleng text">
+                            {menu[itemIndex.Item_ID - 1].Name}
+                          </Card.Text>
+                          <Card.Text className="text">
+                            Quantity: {itemIndex.Quantity}
+                          </Card.Text>
+                          <Card.Text className="maxleng text">
+                            Note: {itemIndex.Item_Note}
+                          </Card.Text>
+                        </Card.Body>
+                      </div>
+                    );
+                })}
               </Card>
             );
         })}
